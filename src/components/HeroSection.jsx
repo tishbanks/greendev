159d64b2aaae861590af602efbe39d8266ebec18
Martin Vanegue <martinvanegue@icloud.com>
2025-05-08 12:45:54 +0200

    Herochange

--- src/components/HeroSection.jsx
+++ src/components/HeroSection.jsx
@@ -52,7 +52,7 @@ export default function HeroSection() {
           {showSecondary && (
             <Subtitle>
               <Typewriter
-                text="Pensé pour durer. Créé à votre image."
+                text="Pensé pour durer. Créé pour vous."
                 speed={30}
                 hideCursor={true}
               />
